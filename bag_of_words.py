45dfc9c946e0b3a169a0e2b92dffc65e2a778dbc
Ixgauth <ixgauth@yahoo.com>
2020-02-17 00:03:54 -0500

    Created bag of words for given .pp script. Should all work from one method call.

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,5 +1,8 @@
 from string import ascii_lowercase
 from string import ascii_uppercase
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def intake_file(filename):
@@ -67,11 +70,37 @@ def split_camel_case(words):
 		words.remove(word)
 	for word in new_words:
 		words.append(word)
-	print(words)
 	return words
 
+def preprocess_data(words):
+	stop_words = stopwords.words('english')
+	new_words = []
+	final_words = []
+	ps = PorterStemmer()
+	for word in words:
+		word = word.lower()
+		if word not in stop_words:
+			new_words.append(word)
 
+	for word in new_words:
+		final_words.append(ps.stem(word))
+	return(final_words)
 
-file_list = intake_file('git-repo/site.pp')
-initial_tokens = split_tokens(file_list)
-split_camel_case(initial_tokens)
+def create_bag_of_words(words):
+	word_freq = {}
+	for word in words:
+		if word not in word_freq:
+			word_freq[word] = 1
+		else:
+			word_freq[word] += 1
+	return(word_freq)
+
+def run_all_bag_of_words(filename):
+	file_list = intake_file(filename)
+	initial_tokens = split_tokens(file_list)
+	new_words = split_camel_case(initial_tokens)
+	final_words = preprocess_data(new_words)
+	bag_of_words = create_bag_of_words(final_words)
+	return bag_of_words
+
+print(run_all_bag_of_words('git-repo/site.pp'))
