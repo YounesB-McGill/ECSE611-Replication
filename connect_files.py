9edbba0868c0982aae325eb1419a48d07077f37c
ixgauth <ixgauth@yahoo.com>
2020-02-19 18:52:55 -0500

    finished way to connect all files at least for mirantis

--- connect_files.py
+++ connect_files.py
@@ -3,11 +3,22 @@ import pandas as pd
 
 def perform_line_cleanup(line):
 	line = line.replace('/Users/akond/PUPP_REPOS/mirantis-downloads/','Mirantis-')
-	print(line)
+	return line
+
+def find_file(filename):
+	os.chdir("../source-code-repos")
+
+	try:
+		file = open(filename, 'r')
+		# print("found file")
+	except FileNotFoundError:
+		print("file not found")
+
 
 filename = "data/IST_MIR.csv"
 df = pd.read_csv(filename, encoding='utf-8', sep=',')
 
 
 for line in df['file_']:
-	perform_line_cleanup(line)
+	cleaned_line = perform_line_cleanup(line)
+	find_file(cleaned_line)
