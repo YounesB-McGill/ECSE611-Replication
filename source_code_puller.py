d93c24d262d075efafd75b8de4b26a0ebf9cf82a
ixgauth <ixgauth@yahoo.com>
2020-02-18 21:36:28 -0500

    generalizing source code puller, stepping away for a min

--- source_code_puller.py
+++ source_code_puller.py
@@ -1,14 +1,16 @@
 from git import Repo
 import os
 
+def clone_repo(repo_name, dir_name)
+	dirName = "mozilla-releng-build-puppet"
+
+	try:
+		os.mkdir(dirName)
+		Repo.clone_from("https://github.com/mozilla-releng/build-puppet", dirName)
+	except OSError:
+		("problem creating directory")
+
+
 os.chdir("../source-code-repos")
 
 print(os.getcwd())
-
-dirName = "mozilla-releng/build-puppet"
-
-try:
-	os.mkdir(dirName)
-	Repo.clone_from("https://github.com/mozilla-releng/build-puppet", dirName)
-except OSError:
-	("problem creating directory")
