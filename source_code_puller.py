9ff4ecc0c11834f96d02bf23b4b18057aa49355e
ixgauth <ixgauth@yahoo.com>
2020-02-19 01:12:52 -0500

    working method of pulling source code from correct date

--- source_code_puller.py
+++ source_code_puller.py
@@ -1,12 +1,20 @@
 from git import Repo
 import os
 
-def clone_repo(repo_name, dir_name)
-	dirName = "mozilla-releng-build-puppet"
+def clone_repo(repo_name):
+	dirName = repo_name.replace('https://github.com/','')
+	dirName = dirName.replace('/','-')
+	print(dirName)
+
+	checkout_command = 'git checkout "`git rev-list master  -n 1 --first-parent --before=2018-10-20`"'
 
 	try:
 		os.mkdir(dirName)
-		Repo.clone_from("https://github.com/mozilla-releng/build-puppet", dirName)
+		Repo.clone_from(repo_name, dirName)
+		os.chdir(dirName)
+		print(os.getcwd())
+		os.system(checkout_command)
+		os.system('git fetch')
 	except OSError:
 		("problem creating directory")
 
@@ -14,3 +22,5 @@ def clone_repo(repo_name, dir_name)
 os.chdir("../source-code-repos")
 
 print(os.getcwd())
+
+clone_repo('https://github.com/openstack/puppet-octavia')
