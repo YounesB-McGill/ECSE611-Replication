257fc1fc9159f391e05fce4f188cc8a1ce28ed25
ixgauth <ixgauth@yahoo.com>
2020-02-22 16:59:25 -0500

    ownership metrics coming along

--- ownership_worker.py
+++ ownership_worker.py
@@ -21,7 +21,23 @@ def get_file_list(df, list_of_files):
 
 	return(list_of_files)
 
-def compute_ownership_metrics(list_of_files):
+def compute_ownership_metrics(file):
+	list_of_authors = {}
+	for author in file:
+		if author in list_of_authors:
+			list_of_authors[author] += 1
+		else:
+			list_of_authors[author] = 1
+	contributers = len(list_of_authors)
+	top_contrib = 0
+	for author in list_of_authors:
+		if list_of_authors[author] > top_contrib:
+			top_contrib = list_of_authors[author]
+	top_contrib = top_contrib/len(file)
+	print(contributers, top_contrib)
+
+	
+
 
 
 df = intake_data_frame("data/repo_commits/Mirantis_fuel-plugin-celebrer.json")
@@ -29,3 +45,7 @@ df = intake_data_frame("data/repo_commits/Mirantis_fuel-plugin-celebrer.json")
 list_of_files = {}
 
 print(get_file_list(df, list_of_files))
+
+for key in list_of_files:
+	print(key)
+	compute_ownership_metrics(list_of_files[key])
