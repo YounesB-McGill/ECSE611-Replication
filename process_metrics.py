1b7abbe8429477e809a50e944b5ece07749c7051
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-03-06 18:30:27 -0500

    Filter files based on whether they occurred in CSV's

--- process_metrics.py
+++ process_metrics.py
@@ -3,13 +3,15 @@
 import datetime
 import json
 import os
+import pandas as pd
 
 from statistics import mean
-from typing import List
+from typing import Dict, List, Tuple
 
 
 REPO_URLS_LOC = "data/repo_commits"
 PROCESS_METRICS_LOC = "data/process_metrics"
+FILTERED_PROCESS_METRICS_LOC = "data/filtered_process_metrics"
 
 class UtcTzinfo(datetime.tzinfo):
     """Helper class to compare dates in different timezones."""
@@ -87,7 +89,7 @@ def compute_average_edit_time(json_entry, script):
     return mean(differences)
 
 
-def compute_process_metrics(json_entry, script):
+def compute_process_metrics(json_entry: dict, script: str, defect_status: int):
     """
     Compute process metrics for a given script.
     """
@@ -98,12 +100,51 @@ def compute_process_metrics(json_entry, script):
             "developers": compute_developers(json_entry, script),
             "fix_commits": compute_fix_related_commits(json_entry, script),
             "age": compute_age(json_entry, script),
-            "avg_edit_time": compute_average_edit_time(json_entry, script)
+            "avg_edit_time": compute_average_edit_time(json_entry, script),
+            "defect_status": defect_status
         }
     }
 
 
 def compute_process_metrics_for_all_scripts():
+    json_entries = get_json_entries()
+
+    repo_pm_entries = []
+    for json_entry in json_entries:
+        print(f"Computing process metrics for {json_entry['url']}")
+        names = json_entry['url'].split("/")
+        proj_name = f"{names[-2]}_{names[-1]}"
+        repo_pm_entry = {
+            "proj_name": proj_name,
+            "url": json_entry['url'],
+            "process_metrics": []
+        }
+        for script in json_entry["pp_files"]:
+            if script is not None:
+                ds = get_defect_status_if_known(proj_name, script)
+                if ds is not None:
+                    repo_pm_entry["process_metrics"].append(
+                        compute_process_metrics(json_entry, script, ds))
+        repo_pm_entries.append(repo_pm_entry)
+    
+    print("Writing process metric info to disk. This may take a while...")
+    for entry in repo_pm_entries:
+        with open(f"{FILTERED_PROCESS_METRICS_LOC}/{entry['proj_name']}.json", "w+") as f:
+            f.write(json.dumps(entry, indent=2))
+
+
+def get_defect_status_if_known(proj_name: str, script: str) -> int:
+    fn_ds = get_csv_filenames_and_defect_status_for_all_projects()[proj_name.split("_")[0]]
+    if "/" in script:
+        script = "/".join(script.split("/")[-2:])
+    for n, d in fn_ds:
+        #print(script, n)
+        if script in n:
+            return d
+
+
+
+def get_json_entries():
     print("Loading repository info from disk. This may take a while...")
     json_entries = []
     for json_file in os.listdir(REPO_URLS_LOC):
@@ -113,26 +154,29 @@ def compute_process_metrics_for_all_scripts():
             except Exception as e:
                 print(f"❌ Failed to load {json_file} with error:\n{e}.")
     print("Loaded repository info to memory.")
+    return json_entries
 
-    repo_pm_entries = []
-    for json_entry in json_entries:
-        print(f"Computing process metrics for {json_entry['url']}")
-        names = json_entry['url'].split("/")
-        repo_pm_entry = {
-            "proj_name": f"{names[-2]}_{names[-1]}",
-            "url": json_entry['url'],
-            "process_metrics": []
-        }
-        for script in json_entry["pp_files"]:
-            if script is not None:
-                repo_pm_entry["process_metrics"].append(
-                    compute_process_metrics(json_entry, script))
-        repo_pm_entries.append(repo_pm_entry)
-    
-    print("Writing process metric info to disk. This may take a while...")
-    for entry in repo_pm_entries:
-        with open(f"{PROCESS_METRICS_LOC}/{entry['proj_name']}.json", "w+") as f:
-            f.write(json.dumps(entry, indent=2))
+
+def get_csv_filenames_and_defect_status_for_all_projects() -> Dict[str, List[Tuple[str, int]]]:
+    return {
+        "Mirantis": get_csv_filenames_and_defect_status("data/IST_MIR.csv"),
+        "mozilla-releng": get_csv_filenames_and_defect_status("data/IST_MOZ.csv"),
+        "openstack": get_csv_filenames_and_defect_status("data/IST_OST.csv"),
+        "wikimedia": get_csv_filenames_and_defect_status("data/IST_WIK.csv")
+    }
+
+def get_csv_filenames_and_defect_status(csv) -> List[Tuple[str, int]]:
+    return list(zip(get_file_list(csv), get_defect_status(csv)))
+
+
+def get_file_list(csv: str) -> List[str]:
+    files = pd.read_csv(csv)["file_"].tolist()
+    files = [fn.split("-downloads/")[1] for fn in files]
+    return files
+
+
+def get_defect_status(csv: str) -> List[int]:
+    return pd.read_csv(csv)["defect_status"].tolist()
 
 
 def get_time(date_string) -> datetime.datetime:
@@ -146,3 +190,6 @@ def is_fix_related(commit_msg: str) -> bool:
 
 if __name__ == "__main__":
     compute_process_metrics_for_all_scripts()
+    #print(
+    #get_csv_filenames_and_defect_status_for_all_projects()
+        #)
