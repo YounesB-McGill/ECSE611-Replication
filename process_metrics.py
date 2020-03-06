09d1942672d523e74ff90ebdd64cc6b197631524
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-03-06 02:06:24 -0500

    Complete computing process metrics

--- process_metrics.py
+++ process_metrics.py
@@ -8,7 +8,8 @@ from statistics import mean
 from typing import List
 
 
-REPO_URLS_LOC = "data/tmp" #"data/repo_commits"
+REPO_URLS_LOC = "data/repo_commits"
+PROCESS_METRICS_LOC = "data/process_metrics"
 
 class UtcTzinfo(datetime.tzinfo):
     """Helper class to compare dates in different timezones."""
@@ -20,6 +21,9 @@ class UtcTzinfo(datetime.tzinfo):
 
 DATE_LIMIT = datetime.datetime(2018, 10, 19, tzinfo=UtcTzinfo())
 
+
+# For large datasets, it is better to combine all this logic in one function
+# so only one iteration is needed 
 
 def compute_commits(json_entry, script):
     count = 0
@@ -42,7 +46,12 @@ def compute_developers(json_entry, script):
 
 
 def compute_fix_related_commits(json_entry, script):
-    pass
+    count = 0
+    for commit in json_entry["commits"]:
+        if script in commit["files"]:
+            if is_fix_related(commit["msg"]):
+                count += 1
+    return count
 
 
 def compute_age(json_entry, script):
@@ -83,11 +92,14 @@ def compute_process_metrics(json_entry, script):
     Compute process metrics for a given script.
     """
     return {
-        "commits": compute_commits(json_entry, script),
-        "developers": compute_developers(json_entry, script),
-        "fix_commits": compute_fix_related_commits(json_entry, script),
-        "age": compute_age(json_entry, script),
-        "avg_edit_time": compute_average_edit_time(json_entry, script)
+        "file": script,
+        "process_metrics": {
+            "commits": compute_commits(json_entry, script),
+            "developers": compute_developers(json_entry, script),
+            "fix_commits": compute_fix_related_commits(json_entry, script),
+            "age": compute_age(json_entry, script),
+            "avg_edit_time": compute_average_edit_time(json_entry, script)
+        }
     }
 
 
@@ -102,30 +114,34 @@ def compute_process_metrics_for_all_scripts():
                 print(f"❌ Failed to load {json_file} with error:\n{e}.")
     print("Loaded repository info to memory.")
 
+    repo_pm_entries = []
     for json_entry in json_entries:
-        print_underline(f"Computing process metrics for {json_entry['url']}")
+        print(f"Computing process metrics for {json_entry['url']}")
+        names = json_entry['url'].split("/")
+        repo_pm_entry = {
+            "proj_name": f"{names[-2]}_{names[-1]}",
+            "url": json_entry['url'],
+            "process_metrics": []
+        }
         for script in json_entry["pp_files"]:
             if script is not None:
-                print(f"Process metrics for {script}:")
-                process_metrics = compute_process_metrics(json_entry, script)
-                print(process_metrics)
-
-        print()
-
-
-def print_underline(string, maxlen=80):
-    """
-    Prints a string and underlines it, up to maxlen dashes. Example
-
-        string
-        ------
-    """
-    print(string)
-    print("-" * min(len(string), maxlen))
+                repo_pm_entry["process_metrics"].append(
+                    compute_process_metrics(json_entry, script))
+        repo_pm_entries.append(repo_pm_entry)
+    
+    print("Writing process metric info to disk. This may take a while...")
+    for entry in repo_pm_entries:
+        with open(f"{PROCESS_METRICS_LOC}/{entry['proj_name']}.json", "w+") as f:
+            f.write(json.dumps(entry, indent=2))
 
 
 def get_time(date_string) -> datetime.datetime:
     return datetime.datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S%z")
+
+
+def is_fix_related(commit_msg: str) -> bool:
+    commit_msg = commit_msg.lower()
+    return "fix" in commit_msg
 
 
 if __name__ == "__main__":
